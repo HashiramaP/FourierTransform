0c56ff2b7df98f255437cb8875947b9be083fb9e
Parsa Homayouni <parsa.homayouni@gmail.com>
2024-10-30 17:12:33 -0400

    modified body colour

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold text-white underline">Hello world!</h1>
+  );
 }
 
 export default App;
